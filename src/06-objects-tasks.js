58ffb2fb9d83df312f6dbe9b7bc1b170579fc1d4
ALEX\alex <lyssenko.alex@gmail.com>
2019-12-16 23:57:19 +0600

    fix: spaces

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -35,7 +35,7 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON( obj ) {
+function getJSON(obj) {
   return JSON.stringify(obj);
 }
 
@@ -51,7 +51,7 @@ function getJSON( obj ) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON( proto, json ) {
+function fromJSON(proto, json) {
   return Object.setPrototypeOf(JSON.parse(json), proto);
 }
 
